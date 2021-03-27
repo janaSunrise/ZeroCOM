640dd7937d4d5ec7a6506c138b8e56767e2c224f
janaSunrise <warriordefenderz@gmail.com>
2021-03-27 16:27:53 +0530

    [WIP] Client, worked on sending mechanism.

--- client/__init__.py
+++ client/__init__.py
@@ -1,9 +1,14 @@
 import socket
 import sys
+import typing as t
 
 from utils.config import HEADER_LENGTH
 from utils.logger import get_logging
 from utils.utils import get_color
+
+
+def get_header(message: bytes) -> bytes:
+    return f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
 
 
 def receive_message(sock: socket.socket) -> tuple:
@@ -20,3 +25,11 @@ def receive_message(sock: socket.socket) -> tuple:
     msg = sock.recv(msg_length).decode('utf-8')
 
     return username, msg
+
+
+def send_message(message: t.Optional[str], sock: socket.socket) -> None:
+    if message:
+        message = message.replace("\n", "").encode("utf-8")
+        message_header = get_header(message)
+
+        sock.send(message_header + message)
