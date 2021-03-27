92491f3bb6e76c4a058887b2225a7235c0940b17
janaSunrise <warriordefenderz@gmail.com>
2021-03-27 14:20:28 +0530

    Added more logging modes.

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,4 @@
 # -- Imports --
-from .logger import get_logging
 from .utils import on_startup
 
 if __name__ == "__main__":
--- server/logger.py
+++ server/logger.py
@@ -1,15 +1,20 @@
 # -- Imports --
 from datetime import datetime
 
+from colorama import Back
+
 from .utils import get_bright_color, get_color
 
 # -- Log mapping --
 log_mapping = {
-    "error": f"[{get_bright_color('RED')}X{get_color('RESET')}]",
+    "error": f"[{get_bright_color('RED')}%{get_color('RESET')}]",
     "warning": f"[{get_bright_color('YELLOW')}!{get_color('RESET')}]",
     "message": f"[{get_bright_color('CYAN')}>{get_color('RESET')}]",
     "success": f"[{get_bright_color('GREEN')}+{get_color('RESET')}]",
+    "info": f"[{get_bright_color('MAGENTA')}#{get_color('RESET')}]",
+    "critical": f"[{get_bright_color('RED')}{Back.YELLOW}X{get_color('RESET')}{Back.RESET}]",
 }
+
 
 def get_logging(type_: str, date: bool = False) -> str:
     message = log_mapping[type_]
