9ae4e9b55295bc12fe58028efa9d08cb7047e7de
janaSunrise <warriordefenderz@gmail.com>
2021-04-01 20:09:45 +0530

    Refactor the Server to use class and OOP principle, and inherit from threading.Thread

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,12 @@
 import socket
+import sys
+import threading
+import time
 import typing as t
 
 from utils.config import HEADER_LENGTH
 from utils.logger import get_logging, get_message_logging
-from utils.utils import get_color
+from utils.utils import get_color, on_startup
 
 
 class Client:
@@ -22,59 +25,114 @@ class Client:
         return f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
 
 
-def receive_message(socket_: socket.socket) -> t.Union[dict, bool]:
-    try:
-        message_header = socket_.recv(HEADER_LENGTH)
+class Server(threading.Thread):
+    def __init__(self, address: tuple) -> None:
+        super().__init__()
 
-        if not len(message_header):
+        self.sockets_list = []
+        self.clients = {}
+
+        self.host = address[0]
+        self.port = address[1]
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self.start_timer = time.perf_counter()
+        self.startup_duration = None
+
+    def connect(self) -> None:
+        try:
+            self.socket.bind((self.host, self.port))
+        except OSError:
+            on_startup("Server")
+            print(get_logging("error", "Server could not be initialized. Check the PORT."))
+            sys.exit(1)
+        else:
+            end = time.perf_counter()
+            duration = round((end - self.start_timer) * 1000, 2)
+
+            on_startup("Server", duration, ip=self.host, port=self.port)
+            self.socket.listen()
+
+            self.sockets_list.append(self.socket)
+
+            print(get_logging("success", "Server started. Listening for connections."))
+
+    def disconnect(self) -> None:
+        for socket_ in self.sockets_list:
+            socket_.close()
+
+    def remove_errored_sockets(self, errored_sockets: list) -> None:
+        for socket_ in errored_sockets:
+            client = self.clients[socket_]
+            print(
+                get_logging(
+                    "warning",
+                    f"{get_color('YELLOW')}Exception occurred. Location {client.username} [{client.address}]"
+                )
+            )
+
+            self.sockets_list.remove(socket_)
+            del self.clients[socket_]
+
+    @staticmethod
+    def receive_message(socket_) -> t.Union[dict, bool]:
+        try:
+            message_header = socket_.recv(HEADER_LENGTH)
+
+            if not len(message_header):
+                return False
+
+            message_length = int(message_header.decode("utf-8").strip())
+
+            return {"header": message_header, "data": socket_.recv(message_length)}
+        except Exception:
             return False
 
-        message_length = int(message_header.decode("utf-8").strip())
+    def process_connection(self) -> bool:
+        socket_, address = self.socket.accept()
 
-        return {"header": message_header, "data": socket_.recv(message_length)}
-    except:
-        return False
+        uname = self.receive_message(socket_)
 
+        client = Client(socket_, address, uname)
 
-def process_conn(sock: socket.socket, sockets: list, clients: dict) -> bool:
-    socket_, address = sock.accept()
+        if not uname:
+            print(get_logging("error", f"New connection failed from {client.address}."))
+            return False
+        else:
+            self.sockets_list.append(socket_)
+            self.clients[socket_] = client
 
-    uname = receive_message(socket_)
+            print(get_logging(
+                "success",
+                f"{get_color('GREEN')}Accepted new connection requested by {client.username} [{client.address}]."
+            ))
+            return True
 
-    client = Client(socket_, address, uname)
+    def process_message(self, socket_) -> bool:
+        def broadcast(message: dict) -> None:
+            for client_socket in self.clients:
+                if client_socket != socket_:
+                    sender_information = client.username_header + client.raw_username
+                    message_to_send = message["header"] + message["data"]
 
-    if not uname:
-        print(get_logging("error", f"New connection failed from {client.address}."))
-        return False
-    else:
-        sockets.append(socket_)
-        clients[socket_] = client
+                    client_socket.send(sender_information + message_to_send)
 
-        print(get_logging(
-            "success",
-            f"{get_color('GREEN')}Accepted new connection requested by {client.username} [{client.address}]."
-        ))
-        return True
+        message = self.receive_message(socket_)
 
+        if not message:
+            client = self.clients[socket_]
 
-def process_message(sock, sockets: list, clients: dict) -> bool:
-    def broadcast(message: dict) -> None:
-        for client_socket in clients:
-            if client_socket != sock:
-                sender_information = client.username_header + client.raw_username
-                message_to_send = message["header"] + message["data"]
-                client_socket.send(sender_information + message_to_send)
+            print(get_logging("error", f"Connection closed [{client.username}@{client.address}]."))
 
-    message = receive_message(sock)
+            self.sockets_list.remove(socket_)
+            del self.clients[socket_]
 
-    if not message:
-        client = clients[sock]
-        print(get_logging("error", f"Connection closed [{client.username}@{client.address}]."))
-        sockets.remove(sock)
-        del clients[sock]
-        return False
+            return False
 
-    client = clients[sock]
-    msg = message['data'].decode('utf-8')
-    print(get_message_logging(client.username, msg))
-    broadcast(message)
+        client = self.clients[socket_]
+        msg = message['data'].decode('utf-8')
+
+        print(get_message_logging(client.username, msg))
+        broadcast(message)
--- server/__main__.py
+++ server/__main__.py
@@ -1,50 +1,35 @@
 # -- Imports --
 import select
-import socket
 import sys
 import time
 
-from utils.utils import on_startup, get_color
 from utils.logger import get_logging
 from utils.config import IP, PORT
-from server import process_conn, process_message
-
-SOCKETS = []
-CLIENTS = {}
+from server import Server
 
 if __name__ == "__main__":
     # -- Perf counter --
     start = time.perf_counter()
 
-    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Initialize the socket object
+    server = Server((IP, PORT))
+    server.start()
 
-    try:
-        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_sock.bind((IP, PORT))
-    except OSError:
-        on_startup("Server")
-        print(get_logging("error", "Server could not be initialized. Check the PORT."))
-        sys.exit(1)
-    else:
-        end = time.perf_counter()
-        duration = round((end - start) * 1000, 2)
+    # Try the connection
+    server.connect()
 
-        on_startup("Server", duration, ip=IP, port=PORT)
-        server_sock.listen()
-
-        SOCKETS.append(server_sock)
-
-        print(get_logging("success", "Server started. Listening for connections."))
-
-    while True:  # Main loop
+    while True:
         try:
-            ready_to_read, _, in_error = select.select(SOCKETS, [], SOCKETS)
+            ready_to_read, _, in_error = select.select(server.sockets_list, [], server.sockets_list)
         except KeyboardInterrupt:
             print(get_logging("info", "Server stopping."))
 
             start = time.perf_counter()
-            for socket in SOCKETS:
-                socket.close()
+
+            # Close the connection
+            server.disconnect()
+
+            # Calculate the timing.
             end = time.perf_counter()
             duration = round((end - start) * 1000, 2)
 
@@ -52,19 +37,10 @@ if __name__ == "__main__":
             sys.exit(0)
 
         for socket_ in ready_to_read:
-            if socket_ == server_sock:
-                process_conn(socket_, SOCKETS, CLIENTS)
+            if socket_ == server.socket:
+                server.process_connection()
             else:
-                process_message(socket_, SOCKETS, CLIENTS)
+                server.process_message(socket_)
 
-        for errored_socket in in_error:
-            client = CLIENTS[errored_socket]
-            print(
-                get_logging(
-                    "warning",
-                    f"{get_color('YELLOW')}Exception occurred. Location {client.username} [{client.address}]"
-                )
-            )
-
-            SOCKETS.remove(errored_socket)
-            del CLIENTS[errored_socket]
+        # Remove errored connections
+        server.remove_errored_sockets(in_error)
