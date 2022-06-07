8680b4fc206385e7a999f5b7f0adeb6ff9c368bd
ItsDrike <itsdrike@protonmail.com>
2022-06-07 18:59:34 +0200

    Remove from_address (only worked for clients)

--- zerocom/protocol/connection.py
+++ zerocom/protocol/connection.py
@@ -1,23 +1,13 @@
 from __future__ import annotations
 
 import socket
-from typing import TYPE_CHECKING
 
 from zerocom.protocol.abc import BaseReader, BaseWriter
-
-if TYPE_CHECKING:
-    from typing_extensions import Self
 
 
 class SocketConnection(BaseReader, BaseWriter):
     def __init__(self, socket: socket.socket):
         self.socket = socket
-
-    @classmethod
-    def from_address(cls, address: tuple[str, int], timeout: float) -> Self:
-        sock = socket.create_connection(address, timeout=timeout)
-        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        return cls(sock)
 
     def read(self, length: int) -> bytearray:
         result = bytearray()
